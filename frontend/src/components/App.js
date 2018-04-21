31bbeb0669daedb723a5b8819e346695b88a1616
Alan Lee <29030938+alanlee1994@users.noreply.github.com>
2018-04-21 10:14:41 +0300

    added the landing page, footer and header

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,14 +3,19 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 //Components
 import Landing from '../components/landing/landing'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
 class App extends Component{
     render(){
         return(
             <BrowserRouter>
                 <div>
+                    <Header/>
                     <Switch>
                         <Route exact path="/" component ={Landing}/>
                     </Switch>
+                    <Footer/>
                 </div>
             </BrowserRouter>
         )
